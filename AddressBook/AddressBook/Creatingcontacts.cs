997db570d0dd4eec7fecb12189eaf892275eac31
badjateamisha <amishabadjate175@gmail.com>
2022-04-27 13:23:12 +0530

    Address Book

---
+++ AddressBook/AddressBook/Creatingcontacts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class CreatingContacts
+    {
+        List<contacts> People = new List<contacts>();
+
+
+
+        public void Contacts()
+        {
+            contacts contact = new contacts();
+
+            Console.WriteLine("Enter First Name : ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Email : ");
+            contact.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number : ");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("\n");
+
+
+            People.Add(contact);
+
+
+        }
+
+        public void output()
+        {
+            foreach (var data in People)
+            {
+
+                Console.WriteLine("Name of the Person : " + data.FirstName + " " + data.LastName);
+                Console.WriteLine("Email ID : " + data.Email);
+                Console.WriteLine("Mobile Number : " + data.PhoneNumber);
+                Console.WriteLine("\n");
+
+            }
+
+        }
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using AddressBook;
+
+Console.WriteLine("Welcom to Address Book Program!");
+
+CreatingContacts contact = new CreatingContacts();
+
+
+
+while (true)
+{
+    Console.WriteLine("Enter the number :\n1)creatingcontacts\n2)Output Details");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            contact.Contacts();
+            break;
+        case 2:
+            contact.output();
+            break;
+    }
+}
