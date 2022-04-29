bcf26fc952320971f96c40e67b2c174431d51168
badjateamisha <99854337+badjateamisha@users.noreply.github.com>
2022-04-29 11:49:30 +0530

    Final data

--- AddressBook/AddressBook/contacts.cs
+++ AddressBook/AddressBook/contacts.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,10 @@ namespace AddressBook
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }    
 
 
     }
