560c20891ae4a95463dd66ee2000471bc51fd5da
badjateamisha <99854337+badjateamisha@users.noreply.github.com>
2022-04-29 11:45:56 +0530

    Final

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -9,14 +9,20 @@ CreatingContacts contact = new CreatingContacts();
 
 while (true)
 {
-    Console.WriteLine("Enter the number :\n1)creatingcontacts\n2)Output Details");
+    Console.WriteLine("Enter the number :\n1)Creatingcontacts\n2)Edit Detaild\n3)Removing Details\n4)Output Details");
     int option = Convert.ToInt32(Console.ReadLine());
-    switch (option)
+    switch(option)
     {
         case 1:
             contact.Contacts();
             break;
         case 2:
+            contact.EditDetails();
+            break;
+        case 3:
+            contact.RemoveContact();
+            break;
+        case 4:
             contact.output();
             break;
     }
